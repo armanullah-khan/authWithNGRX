bf54f832ed844dcb73a73648e3ef1e9bba1b35c9
Your Name <you@example.com>
2019-03-14 00:20:12 -0700

    adding routes to nav components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:SignUpComponent},
+  {path: 'log-in', component:LogInComponent},
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
