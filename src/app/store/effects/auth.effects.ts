e35be3b0edbfcaaf5dd6a9956b18ba0b80ffd08b
Your Name <you@example.com>
2019-03-17 23:03:12 -0700

    effects file created

---
+++ src/app/store/effects/auth.effects.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import { tap } from 'rxjs/operators';
+
+import { AuthService } from '../../services/auth.service';
+
+
+@Injectable()
+export class AuthEffects {
+
+  constructor(
+    private actions: Actions,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  // effects go here
+
+}
