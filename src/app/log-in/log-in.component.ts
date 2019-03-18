c5dbd33d474b0405532b8185148e7444635abbc5
Your Name <you@example.com>
2019-03-17 23:30:13 -0700

    dispatch login action

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../models/user';
 
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/app.states';
+import { LogIn } from '../store/actions/auth.actions';
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
@@ -15,7 +19,8 @@ export class LogInComponent implements OnInit {
 
   user: User = new User();
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private store: Store<AppState>) {
     this.messageForm = this.formBuilder.group({
       email: ['',[Validators.email,Validators.required]],
       password: ['',[Validators.required,Validators.minLength(6)]],
@@ -26,6 +31,13 @@ export class LogInComponent implements OnInit {
   }
   onSubmit(): void {
     this.submitted = true;
+
+    const payload = {
+      email: this.user.email,
+      password: this.user.password
+    };
+    this.store.dispatch(new LogIn(payload));
+    
     console.log(this.user)
     if (this.messageForm.invalid) {
       return;
---
+++ src/app/store/actions/auth.actions.ts
@@ -0,0 +1,14 @@
+import { Action } from '@ngrx/store';
+
+
+export enum AuthActionTypes {
+  LOGIN = '[Auth] Login'
+}
+
+export class LogIn implements Action {
+    readonly type = AuthActionTypes.LOGIN;
+    constructor(public payload: any) {}
+  }
+  
+  export type All =
+    | LogIn;
